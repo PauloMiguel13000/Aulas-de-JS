a18d52d08e4a94e4468e5518dffb24d786b24d04
PauloMiguel13000 <guell.28@hotmail.com>
2022-12-02 11:29:19 -0300

    let, var e const

--- script.js
+++ script.js
@@ -1,22 +1,2 @@
-//setInterval
+//const, nao pode ser alterado o seu valor
 
-function acao(){
-
-document.write("Execultando... <br/>")
-
-}
-/*
-var timer = setInterval(() => {
-  document.write("Execultando!!! <br/>")
-
-}, 1000)
-
-*/
-
-
-//setTimeout
-
-setTimeout(() =>{
-console.log("Execultado");
-
-}, 3000);
